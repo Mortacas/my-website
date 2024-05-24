1b6bf3b4f58ed8cf82de29eb3aee9abed44aadb6
Moises Orta <morta0916@outlook.com>
2024-05-24 13:21:16 -0700

    Grid working and more media queries

--- search.php
+++ search.php
@@ -6,43 +6,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Library Catalog - Available Books</title>
     <link rel="stylesheet" href="css/tableStyles.css">
-    <script src="scripts/searchjavascript.js" defer></script>
-    <script src="scripts/buttons.js" type="text/javascript"></script>
     <link rel="stylesheet" href="css/buttonStyles.css">
 </head>
 
 <body>
     <header>
-
-
         <div class="search-container">
-
-            <a href="index.html"><img src="images/mvpoclogo2.png" alt="OC Logo" class="ocicon"></a>
-            <input type="text" id="searchInput" placeholder="Search available books..."
-                onkeyup="searchAvailableBooks()">
+            <a href="index.php"><img src="images/mvpoclogo2.png" alt="OC Logo" class="ocicon"></a>
+            <input type="text" id="searchInput" placeholder="Search available books...">
             <button type="submit">Request Selected Books</button>
         </div>
-
-
     </header>
 
-
     <main>
-        <form id="bookRequestForm">
+        <form id="bookRequestForm" method="POST" action="">
+            <label for="studentNameInput">Student Name:</label>
+            <input type="text" id="studentNameInput" name="studentName">
+            <br>
+            <label for="studentCtcInput">CTC:</label>
+            <input type="text" id="studentCtcInput" name="studentCtc">
+            <br>
+            <button type="submit" id="searchButton">Search</button>
             <table id="data_table">
                 <thead>
                     <tr>
                         <th>Unit ID</th>
                         <th>Book Name</th>
                         <th>ISBN</th>
-                        <th>Click to add to List for Checkout</th>
+                        <th>Date Signed Out</th>
+                        <th>Date to Return</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <!-- Rows will be dynamically filled by JavaScript -->
+                    <?php include 'common_functions.php'; handleBookSearch(); ?>
                 </tbody>
             </table>
-
         </form>
     </main>
 
